18b1b948cc17a0cc70a234f7ae87d6f662d772a1
djun06 <47366442+djun06@users.noreply.github.com>
2022-04-21 11:06:00 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,10 +13,34 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testLinks() throws IOException {
+    public void testTestFile() throws IOException {
         Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
         List<String> expected = List.of("https://something.com", "some-thing.html");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testExtraChar() throws IOException {
+        Path fileName = Path.of("ExtraChar.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("https://osu.ppy.sh/");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testFarApart() throws IOException {
+        Path fileName = Path.of("FarApart.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of();
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testImage() throws IOException {
+        Path fileName = Path.of("Image.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("https://osu.ppy.sh/");
         assertEquals(expected, MarkdownParse.getLinks(content));
     }
 }
