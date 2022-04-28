9062fae9807af9a0fb879091b3b59f0eefb1c648
djun06 <47366442+djun06@users.noreply.github.com>
2022-04-28 10:40:46 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,9 +17,9 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
-                break;
-            }
+            //if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
+            //    break;
+            //}
             if(markdown.indexOf("!", currentIndex) == openBracket - 1 && openBracket != 0){
                 isImage = true;
             }
